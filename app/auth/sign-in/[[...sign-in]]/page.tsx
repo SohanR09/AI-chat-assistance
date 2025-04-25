bb3d1f291605675e419327542589b19c8c63bbc8
Sohan Rathod <sohan1203@gmail.com>
2025-04-26 02:44:55 +0530

    auth update

--- app/auth/sign-in/[[...sign-in]]/page.tsx
+++ app/auth/sign-in/[[...sign-in]]/page.tsx
@@ -4,10 +4,10 @@ export default function Page() {
   return (
     <div className="flex h-full min-h-[calc(100vh-2rem)] justify-center items-center auth-page">
       <SignIn
-      // path="/auth/sign-in"
-      // routing="path"
-      // signUpUrl="/auth/sign-up"
-      // forceRedirectUrl={"/c"}
+        path="/auth/sign-in"
+        routing="path"
+        signUpUrl="/auth/sign-up"
+        forceRedirectUrl={"/c"}
       />
     </div>
   );
--- app/auth/sign-up/[[...sign-up]]/page.tsx
+++ app/auth/sign-up/[[...sign-up]]/page.tsx
@@ -3,9 +3,7 @@ import { SignUp } from "@clerk/nextjs";
 export default function Page() {
   return (
     <div className="flex h-full min-h-[calc(100vh-2rem)] justify-center items-center mt-4">
-      <SignUp
-      // path="/auth/sign-up" routing="path" signInUrl="/auth/sign-in"
-      />
+      <SignUp path="/auth/sign-up" routing="path" signInUrl="/auth/sign-in" />
     </div>
   );
 }
